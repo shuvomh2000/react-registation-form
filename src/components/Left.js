a01d7b44eedb5e17fa67834e0a2bb71dcb154079
shuvomh2000 <shuvomh2000@gmail.com>
2022-03-24 22:29:40 +0600

    ok

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import {useLocation ,useNavigate } from 'react-router-dom'
-import { DropdownButton,Dropdown,ButtonGroup,Button,ListGroup } from 'react-bootstrap'
+import { DropdownButton,Dropdown,ButtonGroup,Button,ListGroup,Modal,ProgressBar,Form } from 'react-bootstrap'
 import { getAuth, signOut,onAuthStateChanged } from "firebase/auth";
 import { getDatabase, ref, onValue} from "firebase/database";
+import { getStorage, ref as refer, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
 
 
 
@@ -10,8 +12,16 @@ import { getDatabase, ref, onValue} from "firebase/database";
 const Left = (props) => {
     let [users,setUsers] = useState([])
     let [activeuser,setActiveuser] = useState("")
+    let [uploadfile,setUploadfile] = useState('')
+    let [progress2,setProgress2] = useState(0)
+    let [bar,setBar] = useState(false)
+
+
     const auth = getAuth();
     let navigate = useNavigate();
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     let handleLogout = ()=>{
       
@@ -44,6 +54,44 @@ const Left = (props) => {
         });
       },[props.id])
 
+      let handleImageSelect = (e)=>{
+        setUploadfile (e.target.files[0])
+    }
+
+    let handleFileUplaod = ()=>{
+          setProgress2(2)
+          const storage = getStorage();
+          const storageRef = refer(storage, `userpicture/${uploadfile.name}`);
+          const uploadTask = uploadBytesResumable(storageRef, uploadfile);
+
+          uploadTask.on('state_changed', 
+          (snapshot) => {
+              setBar(true)
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setProgress2(progress)
+            console.log('Upload is ' + progress + '% done');
+            if(progress == 100){
+              setUploadfile('')
+              setBar(false)
+              setShow(false)
+            }
+          }, 
+          (error) => {
+            // Handle unsuccessful uploads
+            console.log(error)
+          }, 
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              console.log('File available at', downloadURL);
+            });
+          }
+        );
+    }
+
   return (
     <>
     <div className='left'>
@@ -56,16 +104,33 @@ const Left = (props) => {
             title={props.username}
         >
             <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="2" onClick={handleShow}>Change picture</Dropdown.Item>
             <Dropdown.Item eventKey="3" active>
             Active Item
             </Dropdown.Item>
             <Dropdown.Divider />
             <Dropdown.Item eventKey="4">
             <Button className='w-100 mt-5' onClick={handleLogout} variant="primary" type="submit">Logout </Button>
-                
             </Dropdown.Item>
         </DropdownButton>
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>choose Profile Picture</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <Form.Control onChange={handleImageSelect} type="file" placeholder="massage" />
+            {bar ? < ProgressBar now={progress2?progress2:2} label={`${progress2?progress2:2}%`} /> : ''}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleFileUplaod}>
+                upload
+              </Button>
+            </Modal.Footer>
+        </Modal>
         <h3>peoples</h3>
 
         {users.map(item=>(
--- src/components/Middle.js
+++ src/components/Middle.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { Form } from 'react-bootstrap'
+import { Form,Modal,Button,ProgressBar } from 'react-bootstrap'
 import { getDatabase, ref, set,onValue } from "firebase/database";
+import { getStorage, ref as refer, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
 
 const Middle = () => {
 
     let [msg,setMsg] = useState('')
     let [usermsg,setUsermsg] = useState('')
+    let [uploadfile,setUploadfile] = useState('')
+    let [progress2,setProgress2] = useState(0)
+    let [bar,setBar] = useState(false)
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     let handleMSg = (e)=>{
         setMsg(e.target.value)
@@ -27,6 +37,42 @@ const Middle = () => {
         });
     },[])
 
+    let handleFilesend = (e)=>{
+        setUploadfile (e.target.files[0])
+    }
+    let handleFileUplaod = ()=>{
+        setProgress2(2)
+        const storage = getStorage();
+        const storageRef = refer(storage, `file/${uploadfile.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, uploadfile);
+
+        uploadTask.on('state_changed', 
+        (snapshot) => {
+            setBar(true)
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setProgress2(progress)
+          console.log('Upload is ' + progress + '% done');
+          if(progress == 100){
+            setUploadfile('')
+            setBar(false)
+            setShow(false)
+          }
+        }, 
+        (error) => {
+          // Handle unsuccessful uploads
+          console.log(error)
+        }, 
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+          });
+        }
+      );
+    }
 
   return (
    <>
@@ -34,8 +80,26 @@ const Middle = () => {
     <h1>bdfb</h1>
    </div>
    <Form.Control onChange={handleMSg} type="text" placeholder="massage" />
-   <button onClick={handleSend} className='w-50 mt-2'>Send</button>
-   <button className='w-50'>File</button>
+   <Button className='w-50' onClick={handleSend} >Send</Button>
+   <Button className='w-50' variant="primary" onClick={handleShow}>File</Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>choose a file for upload</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form.Control onChange={handleFilesend} type="file" placeholder="massage" />
+        {bar ? < ProgressBar now={progress2?progress2:2} label={`${progress2?progress2:2}%`} /> : ''}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleFileUplaod}>
+            upload
+          </Button>
+        </Modal.Footer>
+      </Modal>
    </>
   )
 }
